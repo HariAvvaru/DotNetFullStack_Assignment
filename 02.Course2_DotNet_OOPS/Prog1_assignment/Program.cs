19ab1c842574a3a85266b41defe091c66ec66e33
Avvaru Hari Sai Babu <95332303+HariAvvaru@users.noreply.github.com>
2024-09-03 18:08:07 +0530

    Add files via upload

---
+++ 02.Course2_DotNet_OOPS/Prog1_assignment/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, job;
+            int id, sal;
+
+            Console.WriteLine("Enter your id: ");
+            id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your name: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Enter your job: ");
+            job = Console.ReadLine();
+
+            Console.WriteLine("Enter your sal: ");
+            sal = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Employee Details");
+            Console.WriteLine("Employee Id : " + id);
+            Console.WriteLine("Employee Name : " + name);
+            Console.WriteLine("Employee Job : " + job);
+            Console.WriteLine("Employee Sal : " + sal);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 02.Course2_DotNet_OOPS/Prog2_assignment/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int id, unitprice, quant;
+            int tot, disc, fin;
+
+            Console.WriteLine("Enter product id: ");
+            id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter product name: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Enter unit price: ");
+            unitprice = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter quantity: ");
+            quant = int.Parse(Console.ReadLine());
+
+            tot = unitprice * quant;
+
+            if (quant > 50)
+            {
+                disc = (int)(tot * 0.3);
+            }
+            else if (quant > 30)
+            {
+                disc = (int)(tot * 0.2);
+            }
+            else if(quant > 10)
+            {
+                disc = (int)(tot * 0.1);
+            }
+            else
+            {
+                disc = tot;
+            }
+
+            fin = tot - disc;
+
+            Console.WriteLine("Product Details");
+            Console.WriteLine("Product Id : " + id);
+            Console.WriteLine("Product Name : " + name);
+            Console.WriteLine("Unit Price : " + unitprice);
+            Console.WriteLine("Quantity : " + quant);
+            Console.WriteLine("Total Amount : " + tot);
+            Console.WriteLine("Discount Amount : " + disc);
+            Console.WriteLine("Final Amount : " + fin);
+
+            Console.ReadLine();
+        }
+    }
+}
